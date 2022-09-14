40f4f865abd14c376c461e6080b0456578f96327
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-09-14 10:34:05 +0300

    *ngFor

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,6 +1,17 @@
 import {Component} from '@angular/core';
 import {Grade} from "src/app/parent/child/child.component";
 
+interface WeekGrade {
+  id: number;
+  grade: number;
+}
+
+interface Lesson {
+  id: number;
+  title: string;
+  weekGrades: WeekGrade[];
+}
+
 export interface Address {
   city: string;
   street: string;
@@ -13,10 +24,61 @@ export interface Address {
   styleUrls: ['./parent.component.scss']
 })
 export class ParentComponent {
-  grades: string[] = [];
+  // grades: string[] = [];
 
-  getGrade(value: string){
-    this.grades.push(value)
-  }
+  lessons: Lesson[] = [
+    {
+      id: 0,
+      title: 'math',
+      weekGrades: [{
+        id: 0,
+        grade: 5,
+      },
+        {
+          id: 1,
+          grade: 5,
+        },
+        {
+          id: 2,
+          grade: 5,
+        }]
+    },
+    {
+      id: 1,
+      title: 'physic',
+      weekGrades: [{
+        id: 0,
+        grade: 5,
+      },
+        {
+          id: 1,
+          grade: 5,
+        },
+        {
+          id: 2,
+          grade: 5,
+        }]
+    },
+    {
+      id: 2,
+      title: 'English',
+      weekGrades: [{
+        id: 0,
+        grade: 5,
+      },
+        {
+          id: 1,
+          grade: 5,
+        },
+        {
+          id: 2,
+          grade: 5,
+        }]
+    },
+  ]
+
+  // getGrade(value: string){
+  //   this.grades.push(value)
+  // }
 
 }
