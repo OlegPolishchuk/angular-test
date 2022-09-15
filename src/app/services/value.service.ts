6e7bbf2f2ccf7a747b0aa2c53daa192dd2a40b87
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-09-15 11:58:31 +0300

    behaviorSubject

--- src/app/services/value.service.ts
+++ src/app/services/value.service.ts
@@ -1,9 +1,18 @@
 import {Injectable} from "@angular/core";
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root',
 })
 
 export class ValueService {
-  value = 10;
+  value$ = new BehaviorSubject<number>(0)
+
+  add(){
+    this.value$.next(this.value$.getValue() + 1)
+  }
+
+  dec(){
+    this.value$.next(this.value$.getValue() - 1);
+  }
 }
